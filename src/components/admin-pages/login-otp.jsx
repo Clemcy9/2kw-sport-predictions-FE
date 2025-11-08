443060c63dd8586d62e5950065fae1b91e5b3804
victoryduru24 <victoryduru04@gmail.com>
2025-11-08 08:10:37 +0100

    Updated mobile UI

--- src/components/admin-pages/login-otp.jsx
+++ src/components/admin-pages/login-otp.jsx
@@ -62,8 +62,8 @@ export default function LoginOtp () {
     };
     return (
         <div>
-            <div className="min-h-screen flex justify-center items-center bg-white w-full">
-                <div className=" bg-[#C1CAD8] shadow-sm px-4 py-4 space-y-10 flex md:w-[36rem] justify-center items-center flex-col md:px-15 md:my-5 md:py-12">
+            <div className="overflow-y-hidden md:min-h-screen flex justify-center md:items-center bg-white w-full">
+                <div className=" md:bg-[#C1CAD8] shadow-sm px-4 py-8 space-y-10 flex md:w-[36rem] justify-center items-center flex-col md:px-15 md:my-5 md:py-12">
                     <h1
                         className="text-3xl cursor-pointer flex items-center font-bold justify-center font-serif text-[#1A365D] py-5"
                     >
@@ -83,7 +83,7 @@ export default function LoginOtp () {
                     <form onSubmit={handleSubmit} className=" justify-center items-center flex-col flex space-y-8 ">
                         <div className="flex gap-6">
                             {[0,1,2,3].map((_, index) => (
-                                <input type="number" key={index} maxLength={1} onChange={(e) => handleInput(e, index)} onKeyDown={(e) => handleKeyDown(e, index)} ref={(el) => (inputsRef.current[index] = el)} className="bg-[#FCFCFC] border border-[#2743FD] w-[72px] h-[57px] rounded-[0.5rem] focus:ring-2 focus:outline-none focus:ring-[#2743FD] flex justify-center items-center text-center font-semibold font-[sora] text-[#737373] text-lg" />
+                                <input type="number" key={index} maxLength={1} onChange={(e) => handleInput(e, index)} onKeyDown={(e) => handleKeyDown(e, index)} ref={(el) => (inputsRef.current[index] = el)} className="bg-[#FCFCFC] border border-[#2743FD] w-[72px] md:h-[57px] h-[60px] rounded-[0.5rem] focus:ring-2 focus:outline-none focus:ring-[#2743FD] flex justify-center items-center text-center font-semibold font-[sora] text-[#737373] text-lg" />
                             ))}
                         </div>
                         <h3 className="text-[#737373] font-[Montserrat] font-semibold">Input the four digit code sent to your email.</h3>
