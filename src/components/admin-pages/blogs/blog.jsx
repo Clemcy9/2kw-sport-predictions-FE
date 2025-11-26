25fe925825477f768bb32ce46044a7ac3355d7f6
victoryduru24 <victoryduru04@gmail.com>
2025-11-26 14:16:51 +0100

    implemented the blog functionalites so user can create a blog

--- src/components/admin-pages/blogs/blog.jsx
+++ src/components/admin-pages/blogs/blog.jsx
@@ -4,26 +4,32 @@ import { useState } from "react";
 
 export default function NewPost () {
     const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
-    const [media, setMedia] = useState(null);
+    const [body, setbody] = useState("");
+    const [image, setImage] = useState(null);
 
-    const isImage =  (media) => media && media.type?.startsWith("image/");
+    // const isImage =  (image) => image && image.type?.startsWith("image/");
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-    
+
+        const token = localStorage.getItem("authToken");
+
+
 
       try{
-          const res = await fetch("https://twokw-backend.onrender.com/api/blogs", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ title, content, media }),
+          const res = await fetch("https://twokw-backend.onrender.com/api/v1/blogs/", {
+              method: "POST",
+              headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${token}`,},
+               body: JSON.stringify({ title, body, image }),
             });
 
             const data = await res.json();
             console.log("backend response", data);
-
-            
+          console.log("authToken",token);
+        
 
         }catch (ero) {
             console.log(ero)
@@ -38,7 +44,7 @@ export default function NewPost () {
                 <div className="w-full lg:space-y-10 space-y-5">
                      <input type="text" value={title} placeholder="Article Title" onChange={(e) => setTitle(e.target.value)} className="p-3 focus:ring-[#1A365D] outline-none focus:ring-1 border-2 rounded-[0.1em] border-[#00000066] placeholder:text-[#00000066] w-full font-[Inria Sans]" />
 
-                     <textarea value={content} onChange={(e) => setContent(e.target.value)} className="max-h-screen lg:h-[90vh] h-[40vh] w-full p-3 focus:ring-[#1A365D] outline-none focus:ring-1 border-1 rounded-xl border-[#00000066] placeholder:text-[#00000066] font-[Inria Sans]"></textarea>
+                     <textarea value={body} onChange={(e) => setbody(e.target.value)} className="max-h-screen lg:h-[70vh] h-[40vh] w-full p-3 focus:ring-[#1A365D] outline-none focus:ring-1 border-1 rounded-xl border-[#00000066] placeholder:text-[#00000066] font-[Inria Sans]"></textarea>
                 </div>
                 <div className="flex flex-col items-center space-y-10">
                     <button type="submit" className="bg-[#1A365D] text-white w-64 text-xl rounded-xl py-2">
@@ -47,13 +53,13 @@ export default function NewPost () {
                      <div className="flex flex-col justify-center items-center py-4 px-4  rounded-xl bg-[#F5FAFF] ">
                          <h3 className="w-full text-left font-bold text-black/80 font-[Inria Sans]">Feature Image</h3>
                         <article className="border border-[#1A365D] h-50 w-64 rounded-2xl my-4">
-                             {media && isImage(media) && (
-                                <img src={URL.createObjectURL(media)} alt="image" className="w-64 h-50 object-cover rounded-2xl" />
-                             )}
-                             
+                             {/* {image && isImage(image) && (
+                                <img src={URL.createObjectURL(image)} alt="image" className="w-64 h-50 object-cover rounded-2xl" />
+                             )} */}
+                             <input value={image} onChange={(e) => setImage(e.target.value)} className="rounded-xl border text-[#1A365D] font-semibold font-[Open Sans] border-[#1A365D] px-3 py-2 h-50 w-64" placeholder="Enter image Url" />
                         </article>
-                        <input onChange={(e) => setMedia(e.target.files && e.target.files[0] ? e.target.files[0] : null)} id="image-upload" type="file" accept="image/*" className="hidden" />
-                         <label htmlFor="image-upload" className="rounded-xl border text-[#1A365D] font-semibold font-[Open Sans] border-[#1A365D] px-3 py-2">Change Image</label>
+                        {/* <input onChange={(e) => setImage(e.target.files && e.target.files[0] ? e.target.files[0] : null)} id="image-upload" type="file" accept="image/*" className="hidden" />
+                         <label htmlFor="image-upload" className="rounded-xl border text-[#1A365D] font-semibold font-[Open Sans] border-[#1A365D] px-3 py-2">Change Image</label> */}
                     </div>
                 </div>
 
