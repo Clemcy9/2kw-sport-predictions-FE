82b7a855188b2774550cf255c8e6541712440023
victoryduru24 <victoryduru04@gmail.com>
2025-11-28 20:26:47 +0100

    integrated getting fistures

--- src/components/admin-pages/features/fixtures.jsx
+++ src/components/admin-pages/features/fixtures.jsx
@@ -3,74 +3,43 @@ import { FaPlusSquare } from "react-icons/fa";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import { Calendar } from "lucide-react";
 import { Search } from "lucide-react";
+import { useEffect } from "react";
 
 export default function MakePredictions () {
+    
+    const[dropdown, setDropdown] = useState();
+    const [date, setDate] = useState("2025-11-26");
+    const [prediction, setPrediction] = useState([]);
+    const [byName, setByName] = useState("");
+    const [byLeague, setByLeague] = useState("");
+    const [byDate, setByDate] = useState(date);
+        
+          useEffect(() => {
+              fetch(`https://twokw-backend.onrender.com/api/v1/football/fixtures?date=${date}`)
 
-    // const handleDelete = (id) => {
-    //         setPredictions(predictions.filter((item) => item.id !== id));
-            
-    //     };
+              .then((res) => res.json())
+              .then((data) => { 
+        
+                setPrediction(data.data.response || []);
+                // console.log("Updated prediction:",prediction);
+                console.log("API DATA",prediction);
+        
+                console.log("fetched predictions:", data);
+              });
+          }, [date]);
 
-        const [predictions, setPredictions] = useState([
-            {
-                id: 1,
-                league: "Premier League",
-                date: "05 Oct 2025",
-                homeTeam: "Everton",
-                awayTeam: "Burnley",
-                time: "12:30",
-            },
-            {
-                id: 2,
-                league: "Premier League",
-                date: "05 Oct 2025",
-                homeTeam: "Everton",
-                awayTeam: "Burnley",
-                time: "12:30",
-            },
-            {
-                id: 3,
-                league: "Premier League",
-                date: "05 Oct 2025",
-                homeTeam: "Everton",
-                awayTeam: "Burnley",
-                time: "12:30",
-            },
-            {
-                id: 4,
-                league: "Premier League",
-                date: "05 Oct 2025",
-                homeTeam: "Everton",
-                awayTeam: "Burnley",
-                time: "12:30",
-            },
-            {
-                id: 5,
-                league: "Premier League",
-                date: "05 Oct 2025",
-                homeTeam: "Everton",
-                awayTeam: "Burnley",
-                time: "12:30",
-            },
-            {
-                id: 6,
-                league: "Premier League",
-                date: "05 Oct 2025",
-                homeTeam: "Everton",
-                awayTeam: "Burnley",
-                time: "12:30",
-            },
-            {
-                id: 7,
-                league: "Premier League",
-                date: "05 Oct 2025",
-                homeTeam: "Everton",
-                awayTeam: "Burnley",
-                time: "12:30",
-            },
-        ]);
+          const all_predictions = prediction.filter((item) => {
 
-        const[dropdown, setDropdown] = useState();
+              const use_name = item.teams.away.name.toLowerCase().includes(byName.toLowerCase()) || item.teams.home.name.toLowerCase().includes(byName.toLowerCase());
+
+              const use_league = item.league.name.toLowerCase().includes(byLeague.toLowerCase());
+
+              const use_date = byDate ? item.fixture.date.startsWith(byDate) : true;
+
+              return use_name && use_league && use_date;
+
+          })
+
     return(
             <div className="p-4 lg:px-10 lg:min-h-screen flex flex-col w-full">
                 <div className="flex justify-start font-semibold font-sans text-2xl w-full  lg:my-6">
@@ -80,7 +49,7 @@ export default function MakePredictions () {
                     <div className="w-full flex flex-col lg:flex-row lg:justify-between lg:my-4  gap-7 lg:gap-5">
                        
                         <div className="relative lg:w-60 w-full flex justify-between items-center">
-                            <input type="text" placeholder="Search By Name" className="w-full appearance-none border border-[#737373] rounded-[0.9em] py-1 pl-4 pr-10 text-[#737373] focus:ring focus:ring-[#1A365D] focus:border-[#1A365D] outline-none bg-white" />
+                            <input value={byLeague} onKeyDown={(e) => e.key === "Enter key" && all_predictions} onChange={(e) => setByLeague(e.target.value)} type="text" placeholder="Search By Name" className="w-full appearance-none border border-[#737373] rounded-[0.9em] py-1 pl-4 pr-10 text-[#737373] focus:ring focus:ring-[#1A365D] focus:border-[#1A365D] outline-none bg-white" />
                             <Search size={18} className="absolute right-3 top-1/2 -translate-y-1/2 text-[#737373] pointer-events-none" />
                         </div>
                         <h3 className="lg:hidden font-sans font-normal text-sm">
@@ -88,7 +57,7 @@ export default function MakePredictions () {
                         </h3>
                         <div className="w-full lg:w-auto flex lg:gap-4 gap-7">
                             <div className="relative lg:w-60  flex justify-between items-center">
-                            <input type="text" placeholder="Select League" className="w-full appearance-none border border-[#737373] rounded-[0.3em] py-1 pl-4 pr-10 text-[#737373] focus:ring focus:ring-[#1A365D] focus:border-[#1A365D] outline-none bg-white" />
+                        <input value={byName} onKeyDown={(e) => e.key === "Enter key" && all_predictions} onChange={(e) => setByName(e.target.value)} type="text" placeholder="Select League" className="w-full appearance-none border border-[#737373] rounded-[0.3em] py-1 pl-4 pr-10 text-[#737373] focus:ring focus:ring-[#1A365D] focus:border-[#1A365D] outline-none bg-white" />
                             <button onClick={() => setDropdown()}>
                             {dropdown ? (
                              <ChevronUp size={18}  className="absolute right-3 top-1/2 -translate-y-1/2 text-[#737373] pointer-events-none" />
@@ -99,7 +68,7 @@ export default function MakePredictions () {
                             
                            </div>
                            <div className="relative lg:w-60 flex justify-between items-center">
-                            <input type="text" placeholder="11/04/2025" className="w-full appearance-none border border-[#737373] rounded-[0.3em] py-1 pl-4 pr-10 text-[#737373] focus:ring focus:ring-[#1A365D] focus:border-[#1A365D] outline-none bg-white" />
+                        <input value={byDate} onKeyDown={(e) => e.key === "Enter key" && all_predictions} onChange={(e) => setByDate(e.target.value)} type="text" placeholder="11/04/2025" className="w-full appearance-none border border-[#737373] rounded-[0.3em] py-1 pl-4 pr-10 text-[#737373] focus:ring focus:ring-[#1A365D] focus:border-[#1A365D] outline-none bg-white" />
                             <Calendar size={18} className="absolute right-3 top-1/2 -translate-y-1/2 text-[#737373] pointer-events-none" />
                            </div>
                         </div>
@@ -123,17 +92,17 @@ export default function MakePredictions () {
                         </thead>
 
                         <tbody className="text-right">
-                            {predictions.map((item, index) => (
-                                <tr key={item.id} className="leading-tight">
+                            {prediction.map((item, index) => (
+                                <tr key={item.fixture.id} className="leading-tight">
                                     <div className="w-full flex flex-row lg:gap-5 lg:justify-between lg:border-none border p-3 lg:p-0 rounded-xl my-4 lg:my-0 lg:active:hidden active:border-[#1A365D] active:scale-105 active:shadow-xl border-[#1A365D99]">
                                         <div className="flex flex-col  items-start gap-6 lg:flex-row w-full justify-center lg:gap-24">
                                             <td className="lg:py-5 hidden lg:block">{index + 1}</td>
-                                            <td className="lg:py-5">{item.league}</td>
-                                            <td className="lg:py-5"> {item.awayTeam} vs {item.homeTeam}</td>
+                                            <td className="lg:py-5"><img className="w-24 h-24" src={item.league.logo} alt={item.league.name}/> {item.league.name}</td>
+                                            <td className="lg:py-5"><span><img className="w-24 h-24" src={item.teams.away.logo} alt={item.teams.away.name} /> {item.teams.away.name}</span> vs <span><img className="w-24 h-24" src={item.teams.home.logo} alt={item.teams.home.name}/> {item.teams.home.name}</span></td>
                                         </div>
                                         <div className="flex gap-2 font-light font-sans lg:text-lg lg:font-normal text-xs text-[#737373] items-end lg:flex-row flex-col lg:w-full lg:justify-center lg:gap-24">
-                                            <td className="lg:py-5 ">{item.date}</td>
-                                            <td className="lg:py-5 pr-9 lg:pr-0">{item.time}</td>
+                                            <td className="lg:py-5 ">{new Date (item.fixture.date).toLocaleDateString()}</td>
+                                            {/* <td className="lg:py-5 pr-9 lg:pr-0">{new Date(item.fixture.time).toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"})}</td> */}
                                             <td className="lg:py-5 lg:ml-10 lg:pr-0 pr-12">
                                                 <button
                                                     className="text-[#04BA4A] transition"
