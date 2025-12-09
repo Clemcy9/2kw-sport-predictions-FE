a01a37f0e4c1677350cc537acf6f86b8e074ea25
victoryduru24 <victoryduru04@gmail.com>
2025-12-09 01:13:53 +0100

    feat:added required attributes for blog

--- src/components/admin-pages/blogs/blog.jsx
+++ src/components/admin-pages/blogs/blog.jsx
@@ -134,6 +134,7 @@ export default function NewPost() {
         </h2>
         <div className="w-full lg:space-y-5 space-y-5">
           <input
+            required
             type="text"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
@@ -169,6 +170,7 @@ export default function NewPost() {
 
               <div className="relative w-[150px] h-[40px] py-2 px-6 border border-[#1A365D] rounded-xl cursor-pointer text-[#1A365D] font-semibold font-[Open Sans]">
                 <input
+                  required
                   type="file"
                   onChange={handleFileChange}
                   hidden
